74a465592924b50c5a5de7298bbe15b6ae8da3c1
epic <ian.angola@davinci.edu.ar>
2020-10-30 18:17:26 -0300

    Destriur y Clonar, primera instancia

---
+++ Assets/Scripts/Editor/WindowSelector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class WindowSelector : EditorWindow
+{
+    [MenuItem("Window/Prefab Selector")]
+    public static void ShowWindow()
+    {
+        GetWindow<WindowSelector>().Show();
+    }
+
+    private void OnGUI()
+    {
+        var GUIDs = AssetDatabase.FindAssets("t:gameobject");
+        foreach (var GUID in GUIDs)
+        {
+            string path = AssetDatabase.GUIDToAssetPath(GUID);
+            var tex = AssetPreview.GetAssetPreview(AssetDatabase.LoadAssetAtPath<GameObject>(path));
+
+            GUILayout.Label(new GUIContent(tex));
+        }
+
+    }
+}
+
